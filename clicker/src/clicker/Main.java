fb41daf2bc687a0f944e16a868d9412855aac370
sean <smsod18@gmail.com>
2015-04-08 15:42:59 -0500

    Pushed bare skeleton of NetBeans project structure. Will add on the view package soon, feel free to add model and controller whenever you want.

---
+++ clicker/src/clicker/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clicker;
+
+/**
+ *
+ * @author sean chris rodney
+ * Cool GUI for clicking to your heart's content.
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
