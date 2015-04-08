8d8cb20ad2f0272c47f6c166b5f18d3af1ab7b76
rodneyxr <rodneyxr@gmail.com>
2015-04-07 23:47:05 -0500

    added skele for main

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,7 +1,13 @@
 package core;
+
+import core.controller.Controller;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Model model = new Model();
+		Controller controller = new Controller(model);
+		View view = new View(model, controller);
 		System.out.println("Hello world!");
 	}
 
