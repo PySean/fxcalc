3523e2109385521b6ed793f570fe8c813dc960b8
continuumNought <lavalle.l.c@gmail.com>
2015-04-11 13:45:11 -0500

    the structurez

---
+++ src/ButtonClickerModel.py
@@ -0,0 +1,10 @@
+from PyQt4.QtCore import QObject
+
+class ButtonClickerModel(QObject):
+   def __init__(self):
+      super().__init__()
+      this.clicks = 0
+
+   def incrementClicks(self):
+      self.clicks += 1
+      #TODO : S
